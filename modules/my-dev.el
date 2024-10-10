f0626b3ea82b9e54a7a845b7d38df67350ab0c34
Jon Harris <jonathan.harris@mlprograms.com>
2024-10-10 12:39:12 +0200

    [dev] Add projectile-mode

--- modules/my-dev.el
+++ modules/my-dev.el
@@ -11,6 +11,15 @@
   (let ((vterm-buffer-name (read-string "Enter new vterm buffer name: ")))
     (vterm (generate-new-buffer-name (concat "*" vterm-buffer-name "*"))))))
 
+;; To provide project management + navigation features
+(use-package projectile
+  :init (projectile-mode 1)
+  :custom
+  ;; Cache to prevent slow 'projectile-find-file' on larger projects
+  (projectile-enable-caching t)
+  :bind (:map projectile-mode-map
+              ("C-c p" . projectile-command-map)))
+
 ;; Update indentation in response to changes to code
 (use-package aggressive-indent
   :hook (emacs-lisp-mode))
