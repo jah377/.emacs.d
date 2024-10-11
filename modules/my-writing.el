f5bac72ce792fc7b15be80e19ea246a9cf8fbde9
Jon Harris <jonathan.harris@mlprograms.com>
2024-10-11 17:30:54 +0200

    [writing] Add denote package

--- modules/my-writing.el
+++ modules/my-writing.el
@@ -29,6 +29,27 @@
   (add-to-list 'vertico-multiform-categories
                '(jinx grid (vertico-grid-annotate . 20))))
 
+(use-package denote
+  :commands denote
+  :custom
+  (denote-directory "~/notes/")
+  (denote-file-type "org")
+  (denote-prompts '(title keywords))
+  (denote-known-keywords '("emacs" "python" "linux" "ml" "work"))
+  ;; TODO: use separate templates for coding/ect
+  (denote-templates nil)
+  (denote-org-front-matter (concat "#+TITLE: %1$s\n"
+                                   "#+DATE: %2$s\n"
+                                   "#+ID: %4$s\n"
+                                   "#+FILETAGS: %3$s\n"
+                                   "#+STARTUP: overview\n"))
+  :config
+  ;; 'denote' provides fuctionality to organize links and backlinks in
+  ;; 'org-dblock'. Good idea to update before each save.
+  (add-hook 'before-save-hook (lambda ()
+                                (when (derivedmode-p 'org-mode)
+                                  (org-update-all-dblocks)))))
+
 (provide 'my-writing)
 
 ;;; my-writing.el ends here
