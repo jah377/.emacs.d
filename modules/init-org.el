fd9a27516ed2d6e472ec71e9d3fe81552b70fe06
Jon Harris <jonathan.harris@mlprograms.com>
2024-12-05 05:40:38 +0100

    [org] Fix org-auto-tangle
    - `:after org-mode`, not `:after org`
    - Reword package notes

--- modules/init-org.el
+++ modules/init-org.el
@@ -61,9 +61,8 @@
              (looking-back "^\**"))))
 
 (use-package org-auto-tangle
-  :after org
-  :hook (org-mode . org-auto-tangle-mode)
-  :custom (org-auto-tangle-default nil))
+  :after org-mode
+  :hook (org-mode . org-auto-tangle-mode))
 
 (org-babel-do-load-languages 'org-babel-load-languages
                              '((emacs-lisp . t)
