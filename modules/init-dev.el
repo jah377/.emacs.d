4e2e6bf31372b593e7bd2cc7cdb0f636953e9801
Jon Harris <jonathan.harris@mlprograms.com>
2025-02-24 17:15:44 +0100

    [lsp] Add tools venv to exec-path

--- modules/init-dev.el
+++ modules/init-dev.el
@@ -187,7 +187,9 @@
 (use-package exec-path-from-shell
   :defer t
   :config
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  ;; Environment containing LSP packages
+  (add-to-list 'exec-path (expand-file-name "~/.virtualenvs/tools/bin")))
 
 (use-package jupyter
   :demand t
